ed4871674b0508e6d87833091e90aabeaf73373a
Kotikov <11deadsluts@gmail.com>
2023-12-15 19:48:50 +0300

    it's done!

--- src/main/java/org/homework/Main.java
+++ src/main/java/org/homework/Main.java
@@ -1,7 +1,35 @@
 package org.homework;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> numbers = generateRandomNumbers(10, 1, 100);
+
+        System.out.println("Список случайных чисел:");
+        numbers.forEach(System.out::println);
+
+        double average = numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("Среднее значение всех четных чисел: " + average);
+    }
+
+    private static List<Integer> generateRandomNumbers(int size, int minRange, int maxRange) {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            int randomNumber = random.nextInt(maxRange - minRange + 1) + minRange;
+            randomNumbers.add(randomNumber);
+        }
+
+        return randomNumbers;
+
     }
 }
